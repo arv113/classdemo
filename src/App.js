2d6c94bc18ded52578a766c40b570ba7de227b5b
April V <arv113@txstate.edu>
2024-03-21 13:21:28 -0500

    Class changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import CatFact from "./CatFact.js";
 
 //for building react components there are 2 syntaxes
 // class model or function model --> 2 sets of syntax where you can do the same thing
 // function method is the preferred methods atm --> more concise method but little less control
 // rick requires us to learn both and will specify when
 // see examples on Canvas rick linked
+//look at Starting Template.html to see CDN method
+//pointing to 3 js files
+//then script block with type babel (for babel compiler to translate the js)
+//defines the JS component
 
+//App.js is the main component
 //shows what an "App" is
 //function called App lol
 function App() {
@@ -17,6 +23,7 @@ function App() {
       {" "}
       {/* className for styling purposes*/}
       <header className="App-header">
+        <CatFact />
         <h1>Bitch</h1> {/* Adding h1 in here */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
